d5d65753b39d0dd38b7bcedacd86549ec6ab6595
MOHD-REHMAN <rehman28.cse14@gmail.com>
2017-02-14 14:56:41 +0530

    Update

--- mobile/MyApp/app/src/main/java/com/prospective/user/myapp/MainActivity.java
+++ mobile/MyApp/app/src/main/java/com/prospective/user/myapp/MainActivity.java
@@ -29,10 +29,48 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
 
+            HttpResponse response = null;
+            try {
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(
+                        "http://subbu/pa/admin/api/v1.0/controller/user-controller.php?action=userRegister  "));
+                response = client.execute(request);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
+            String responseText = null;
+            try {
+                responseText = EntityUtils.toString(response.getEntity());
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Log.i("Parse Exception", e + "");
 
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Log.i("IO Exception 2", e + "");
+
+            }
+
+            Log.i("responseText", responseText);
+
+            Toast.makeText(MainActivity.this, responseText + "",
+                    Toast.LENGTH_SHORT).show();
 
         }
+
+
+
+    }
     });
 
     }
