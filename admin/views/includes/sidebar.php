dd5b1b66cd60425df60c557a3e45d687acd82935
utkarsh <utkarshjha@mail.com>
2017-02-16 15:03:36 +0530

    minor changes

--- admin/views/includes/sidebar.php
+++ admin/views/includes/sidebar.php
@@ -4,8 +4,7 @@ include_once "dboard-css-files.php";
 //echo ADMIN_BASE_URL;
 ?>
 <link rel="stylesheet" type="text/css" href="../../assets/css/dboard-style.css">
-<div class="container-fluid">
-<div class="row">
+
 <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 dboard-header"></div>
 <div class="col-lg-2 col-md-3 col-sm-12 col-xs-12 dboard-sidebar">
 	<ul>
@@ -17,8 +16,6 @@ include_once "dboard-css-files.php";
 	    <li><a href="<?php echo ADMIN_BASE_URL;?>views/feedback/feedback-index.php">Feedback</a></li>
 	    <li><a href="../../system/controller/login-controller.php?action=logout">logout</a></li>
 	</ul>	
-</div> 
-</div>
 </div>
 
 
