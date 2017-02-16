084b9d15b859f0daf59d262efb652597e9e1594e
utkarsh <utkarshjha@mail.com>
2017-02-16 14:55:09 +0530

    minor changes

--- admin/views/includes/dboard-css-files.php
+++ admin/views/includes/dboard-css-files.php
@@ -1,2 +1,1 @@
 <link rel="stylesheet" type="text/css" href="../../assets/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="../../assets/css/dboard-style.css">
--- admin/views/includes/sidebar.php
+++ admin/views/includes/sidebar.php
@@ -3,6 +3,7 @@ include_once "../../system/library/application.php";
 include_once "dboard-css-files.php";
 //echo ADMIN_BASE_URL;
 ?>
+<link rel="stylesheet" type="text/css" href="../../assets/css/dboard-style.css">
 <div class="container-fluid">
 <div class="row">
 <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 dboard-header"></div>
