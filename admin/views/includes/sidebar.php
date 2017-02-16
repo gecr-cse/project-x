96329daee716b32a02d72baac8f2f6eda8617303
utkarsh <utkarshjha@mail.com>
2017-02-16 13:35:53 +0530

    Revert "changes made into admin->includes->sidebar"
    This reverts commit cfda2aad5df565cf25de713392b9524a722c42ba.

--- admin/views/includes/sidebar.php
+++ admin/views/includes/sidebar.php
@@ -1,19 +1,15 @@
 <?php
 include_once "../../system/library/application.php";
-include_once "css-files.php";
-//echo ADMIN_BASE_URL;
+echo ADMIN_BASE_URL;
 ?>
-<div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 dboard-header"></div>
-<div class="col-lg-2 col-md-3 col-sm-12 col-xs-12 dboard-sidebar">
-	<ul>
-	    <li><a href="<?php echo ADMIN_BASE_URL;?>views/dashboard/dashboard-index.php">DASH BOARD</a></li>
-	    <li><a href="<?php echo ADMIN_BASE_URL;?>views/student/student-index.php">Students</a></li>
-	    <li><a href="<?php echo ADMIN_BASE_URL;?>views/department/department-index.php">Department</a></li>
-	    <li><a href="<?php echo ADMIN_BASE_URL;?>views/news/news-index.php">News</a></li>
-	    <li><a href="<?php echo ADMIN_BASE_URL;?>views/issues/issues-index.php">Issues</a></li>
-	    <li><a href="<?php echo ADMIN_BASE_URL;?>views/feedback/feedback-index.php">Feedback</a></li>
-	    <li><a href="../../system/controller/login-controller.php?action=logout">logout</a></li>
-	</ul>	
-</div> 
 
+<ul>
+    <li><a href="<?php echo ADMIN_BASE_URL;?>views/dashboard/dashboard-index.php">DASH BOARD</a></li>
+    <li><a href="<?php echo ADMIN_BASE_URL;?>views/student/student-index.php">Students</a></li>
+    <li><a href="<?php echo ADMIN_BASE_URL;?>views/department/department-index.php">Department</a></li>
+    <li><a href="<?php echo ADMIN_BASE_URL;?>views/news/news-index.php">News</a></li>
+    <li><a href="<?php echo ADMIN_BASE_URL;?>views/issues/issues-index.php">Issues</a></li>
+    <li><a href="<?php echo ADMIN_BASE_URL;?>views/feedback/feedback-index.php">Feedback</a></li>
+    <li><a href="../../system/controller/login-controller.php?action=logout">logout</a></li>
 
+</ul>
