67decef2d920a9b3c889ad2f948fac73539b3339
div2013 <divyanshu20.cse13@gmail.com>
2017-02-18 11:28:08 +0530

    attempting commit

---
+++ admin/views/includes/navbar.php
@@ -0,0 +1,19 @@
+<?php
+include_once "../../system/library/application.php";
+include_once "dboard-css-files.php";
+//echo ADMIN_BASE_URL;
+?>
+<!--
+<link rel="stylesheet" type="text/css" href="../../assets/css/dboard-style.css">
+
+<div class="row">
+<div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 dboard-header"></div>
+</div>-->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Icon</a>
+    </div>
+  
+  </div>
+</nav>
